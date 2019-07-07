110a05a8d601c5cc9a50050b92340ff248eaf686
aawadall <aawadall@ualberta.ca>
2019-07-07 07:17:17 -0400

    Model package and object

---
+++ src/main/java/com/aawadall/model/Customer.java
@@ -0,0 +1,42 @@
+package com.aawadall.model;
+
+public class Customer {
+    private String firstName;
+    private String lastName;
+
+    /**
+     * Empty Constructor
+     */
+    public Customer() {
+
+    }
+
+    /**
+     * @return String return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return String return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
