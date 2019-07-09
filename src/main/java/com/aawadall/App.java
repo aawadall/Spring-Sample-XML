cfbccf11eeaeb584af719ef9e97f481cedbe7410
Ahmed Awadallah <aawadall@ualberta.ca>
2019-07-09 14:40:51 -0400

    debugging step to know where am i

--- src/main/java/com/aawadall/App.java
+++ src/main/java/com/aawadall/App.java
@@ -1,5 +1,7 @@
 package com.aawadall;
 
+import java.io.File;
+
 import com.aawadall.service.CustomerService;
 
 import org.springframework.context.ApplicationContext;
@@ -13,9 +15,21 @@ public class App {
     public static void main(String[] args) {
         // CustomerService service = new CustomerServiceImpl();
 
-        ApplicationContext appContext = new ClassPathXmlApplicationContext("resources/applicationConext.xml");
+        File folder = new File(".");
+        File[] listOfFiles = folder.listFiles();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                System.out.println("File " + listOfFiles[i].getName());
+            } else if (listOfFiles[i].isDirectory()) {
+                System.out.println("Directory " + listOfFiles[i].getName());
+            }
+        }
+        /*
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("../../applicationConext.xml");
         CustomerService service = appContext.getBean("customerService", CustomerService.class);
         System.out.println("Find First Customer");
         System.out.println(service.findAll().get(0).getFirstName());
+        */
     }
 }
