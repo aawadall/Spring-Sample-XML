42d69620b04f7c9cc3e11559a3d8ee9699ff48a0
Ahmed Awadallah <aawadall@ualberta.ca>
2019-07-09 16:08:04 -0400

    EOD

--- src/main/java/com/aawadall/App.java
+++ src/main/java/com/aawadall/App.java
@@ -14,18 +14,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         // CustomerService service = new CustomerServiceImpl();
-
-        File folder = new File(".");
-        File[] listOfFiles = folder.listFiles();
-
-        for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].isFile()) {
-                System.out.println("File " + listOfFiles[i].getName());
-            } else if (listOfFiles[i].isDirectory()) {
-                System.out.println("Directory " + listOfFiles[i].getName());
-            }
-        }
-        
+   
         ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         CustomerService service = appContext.getBean("customerService", CustomerService.class);
         System.out.println("Find First Customer");
