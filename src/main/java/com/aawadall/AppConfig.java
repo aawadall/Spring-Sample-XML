a3dade34ead3f7f52cf086a8c12caf6565f558ee
Ahmed Awadallah <aawadall@ualberta.ca>
2019-07-12 10:05:55 -0400

    app config has no manual wirings!

--- src/main/java/com/aawadall/AppConfig.java
+++ src/main/java/com/aawadall/AppConfig.java
@@ -14,14 +14,14 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan({"com.aawadall"})
 public class AppConfig {
 
-    @Bean(name = "customerService")
-    public CustomerService getCustomerService() {
-        //CustomerServiceImpl service = new CustomerServiceImpl(getCustomerRepository());
-        CustomerServiceImpl service = new CustomerServiceImpl();
-        //service.setCustomerRepository(getCustomerRepository());
-        System.out.println("AppConfig::getCustomerConfig");
-        return service;
-    }
+    // @Bean(name = "customerService")
+    // public CustomerService getCustomerService() {
+    //     //CustomerServiceImpl service = new CustomerServiceImpl(getCustomerRepository());
+    //     CustomerServiceImpl service = new CustomerServiceImpl();
+    //     //service.setCustomerRepository(getCustomerRepository());
+    //     System.out.println("AppConfig::getCustomerConfig");
+    //     return service;
+    // }
 
     // @Bean(name = "customerRepository")
     // public CustomerRepository getCustomerRepository() {
