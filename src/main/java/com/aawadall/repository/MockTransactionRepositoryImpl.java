45e05d95ad656d54d43fa964f831a304287caa7b
aawadall <aawadall@ualberta.ca>
2019-07-13 10:40:27 -0400

    implment mock transaction repository

---
+++ src/main/java/com/aawadall/repository/MockTransactionRepositoryImpl.java
@@ -0,0 +1,16 @@
+package com.aawadall.repository;
+
+import java.util.List;
+
+import com.aawadall.model.Transaction;
+
+public class MockTransactionRepositoryImpl implements TransactionRepository {
+
+    @Override
+    public List<Transaction> getTransactions() {
+        // TODO: implement get transactions
+        throw new UnsupportedOperationException();
+
+    }
+
+}
